09d558bcbb65bea71d06102e9d96c3cc9d78a2cf
Martin Agoha <martin4dtruth@gmail.com>
2023-04-09 22:57:13 +0100

    fixed bug

--- my_app.py
+++ my_app.py
@@ -5,19 +5,20 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 
+#class
 class childApp(GridLayout):
     def __init__(self, **kwargs):
         super(childApp, self).__init__()
         self.cols = 2
-        self.add_widget(Label(text = 'Student Name'))
+        self.add_widget(Label(text = 'Student' Name'))
         self.s_name = TextInput()
         self.add_widget(self.s_name)
         
-        self.add_widget(Label(text = 'Student Marks'))
+        self.add_widget(Label(text = 'Student' Marks'))
         self.s_marks = TextInput()
         self.add_widget(self.s_marks)
 
-        self.add_widget(Label(text = 'Student Gender'))
+        self.add_widget(Label(text = 'Student' Gender'))
         self.s_gender = TextInput()
         self.add_widget(self.s_gender)
 
